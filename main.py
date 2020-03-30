bdf3f87be78af78f2342704cc93f3fde3cc8bf2b
Nick Cottrell <nrc1mr.e3@gmail.com>
2020-03-30 14:23:54 -0400

    Reformatted everything to standard convention (#17)

--- main.py
+++ main.py
@@ -3,22 +3,22 @@ import logging
 import os
 import re
 
-from modules.corrections import JSON, connectDict, correctValue
-from modules.gui import InstallError, PopupTag, mainGUI
-from modules.imageScraper import imageScraper
-from modules.sanity import checkBlankRow, sanityName
+from modules.corrections import JSON, connect_dict, correct_value
+from modules.gui import InstallError, MainGUI, PopupTag
+from modules.image_scraper import image_scraper
+from modules.sanity import check_blank_row, sanity_name
 
 # if opencv isnt installed, it'll install it for you
 try:
     import numpy as nm
     import cv2
 except ImportError:
-    if(os.system("pip install opencv-python")):
+    if os.system("pip install opencv-python"):
         os.system("pip install --user opencv-python")
 try:
     from PIL import Image, ImageTk
 except ModuleNotFoundError:
-    if(os.system("pip install pillow")):
+    if os.system("pip install pillow"):
         os.system("pip install --user pillow")
 except ImportError:
     import Image
@@ -28,14 +28,14 @@ except ImportError:
 try:
     import pytesseract as tess
 except ImportError:
-    if(os.system("pip install pytesseract")):
+    if os.system("pip install pytesseract"):
         os.system("pip install --user pytesseract")
     import pytesseract as tess
 # installing pdf to image libraries
 try:
     from pdf2image import convert_from_path
 except ImportError:
-    if(os.system("pip install pdf2image")):
+    if os.system("pip install pdf2image"):
         os.system("pip install --user pdf2image")
     from pdf2image import convert_from_path
 
@@ -69,28 +69,32 @@ elif "debug" in os.sys.argv:
     else:
         os.system("del /s debugOutput\\*.jpg")
 
-JSONFile = open("./aliases.json", "r")
-connectDict(json.load(JSONFile))
-JSONFile.close()
-JSONChange = False  # this is only used when the database is updated
-mainDisplay = None
+JSON_FILE = open("./aliases.json", "r")
+connect_dict(json.load(JSON_FILE))
+JSON_FILE.close()
+JSON_CHANGE = False  # this is only used when the database is updated
+Main_Display = None
 
 
 def debug(label: str, content: list):
     logging.debug("%s:", label)
-    if(logging.getLogger().level <= logging.DEBUG):
+    if (logging.getLogger().level <= logging.DEBUG):
         for i in content:
             print(i)
 
 
-def debugImageDictionary(diction):
+def debug_image_dictionary(diction):
+    """ This debugs the image dictionary. It takes a given image dictionary and makes
+    a table organized format of images in a debugging folder
+    """
     if (logging.getLogger().level <= logging.INFO):
-        debugOutput = "Sheet | SheetLen | TableRow | TableCol\n"
+        debug_output = "Sheet | SheetLen | TableRow | TableCol\n"
         for sheet in range(len(diction)):
-            debugOutput += "{ind: 5d} | {slen: 8d} | {trow: 8d} | {tcol: 8d}\n".format(ind=sheet, slen=len(
-                diction[sheet]), trow=len(diction[sheet][1]), tcol=len(diction[sheet][1][0]))
-        logging.info(debugOutput)
-        exportToFile("debugOutput/dictionaryStats.txt", debugOutput)
+            debug_output += "{ind: 5d} | {slen: 8d} | {trow: 8d} | {tcol: 8d}\n".format(
+                ind=sheet, slen=len(diction[sheet]),
+                trow=len(diction[sheet][1]), tcol=len(diction[sheet][1][0]))
+        logging.info(debug_output)
+        export_to_file("debugOutput/dictionaryStats.txt", debug_output)
         for sheet in range(len(diction)):
             for dates in range(len(diction[sheet][0])):
                 cv2.imwrite("debugOutput/dictionary/sheet{sheet}date{date}.jpg".format(
@@ -101,57 +105,57 @@ def debugImageDictionary(diction):
                         sheet=sheet, row=row, col=col), diction[sheet][1][row][col])
 
 
-def exportToFile(dir, content):
-    open(dir, "w").write(content)
+def export_to_file(file, content):
+    open(file, "w").write(content)
 
 
-def appendToFile(dir, content):
+def append_to_file(file, content):
     try:
-        inside = open(dir, "r").read()
-        open(dir, "w").write(inside + content)
+        inside = open(file, "r").read()
+        open(file, "w").write(inside + content)
     except:
-        open(dir, "w").write(content)
+        open(file, "w").write(content)
 
 
-def TranslateDictionary(sheetsDict, gui=False, outputDict=None):
-    """ Phase two of plan. This function goes through the image dictionary passed 
+def translate_dictionary(sheets_dict, gui=False, output_dict=None):
+    """ Phase two of plan. This function goes through the image dictionary passed
     to it and creates a matrix of the dictionary in text.\n
     @param sheetsDict: a matrix of images made from a table.\n
     @param gui: whether to switch on global gui manipulation for the progress bar.\n
-    @param outputDict: a variable passed by reference instead of using return.\n
+    @param output_dict: a variable passed by reference instead of using return.\n
     @return a matrix of strings that represents the text in the image dictionary.
     """
     global JSON
-    global JSONChange
-    results = [[] for x in sheetsDict]  # results the size of pages in dict
+    global JSON_CHANGE
+    results = [[] for x in sheets_dict]  # results the size of pages in dict
 
     # GUI widgets to manipulate while in middle of function
     if(gui):
-        sheetMax = len(sheetsDict)
-        sheetInd = 0
-        rowInd = 0
-        progressMax = 1
+        sheet_max = len(sheets_dict)
+        sheet_ind = 0
+        row_ind = 0
+        progress_max = 1
 
         # Gui Texts
-        textScan = "Scanning\tSheet: {sInd} of {sMax}\tRow: {rInd} of {rMax}"
-        textSanitize = "Sanitizing\tSheet: {sInd} of {sMax}\tRow: {rInd} of {rMax}"
+        text_scan = "Scanning\tSheet: {sInd} of {sMax}\tRow: {rInd} of {rMax}"
+        text_sanitize = "Sanitizing\tSheet: {sInd} of {sMax}\tRow: {rInd} of {rMax}"
 
         # Getting max for progress bar
-        for sheet in sheetsDict:
-            progressMax += len(sheet[1]) - 1
-        mainDisplay.progressBar.configure(
-            mode="determinate", maximum=progressMax)
+        for sheet in sheets_dict:
+            progress_max += len(sheet[1]) - 1
+        Main_Display.progress_bar.configure(
+            mode="determinate", maximum=progress_max)
 
     # Collecting data to database
-    for sheet in range(len(sheetsDict)):
+    for sheet in range(len(sheets_dict)):
         if gui:
-            sheetInd += 1
-            rowMax = len(sheetsDict[sheet][1]) - 1
+            sheet_ind += 1
+            row_max = len(sheets_dict[sheet][1]) - 1
         # Collecting dates on page first
         dates = []
         dformat = re.compile(r'\d{1,2}\/\d{1,2}\/(\d{4}|\d{2})')
         dstr = ""
-        for date in sheetsDict[sheet][0]:
+        for date in sheets_dict[sheet][0]:
             dstr = tess.image_to_string(date).replace(
                 "\n", "").replace(" ", "")
             if (bool(dformat.match(dstr))):
@@ -161,64 +165,69 @@ def TranslateDictionary(sheetsDict, gui=False, outputDict=None):
 
         # | Full name | Time in | Time out | hours (possibly blank) | purpose | date | day (possibly blank) |
         # skips first row which is dummy
-        for row in range(1, len(sheetsDict[sheet][1])):
+        for row in range(1, len(sheets_dict[sheet][1])):
             if gui:
-                rowInd += 1
-                mainDisplay.progressBar.step()
-                mainDisplay.sheetStatus.configure(
-                    text=textScan.format(sInd=sheetInd, sMax=sheetMax, rInd=rowInd, rMax=rowMax))
-                mainDisplay.root.update_idletasks()
+                row_ind += 1
+                Main_Display.progress_bar.step()
+                Main_Display.sheet_status.configure(
+                    text=text_scan.format(sInd=sheet_ind, sMax=sheet_max,
+                                          rInd=row_ind, rMax=row_max))
+                Main_Display.root.update_idletasks()
             results[sheet].append([None for x in range(5)])  # array of 5 slots
             # skip first col which is dummy
-            for col in range(1, len(sheetsDict[sheet][1][row])):
+            for col in range(1, len(sheets_dict[sheet][1][row])):
                 logging.info("Sheet[%d]: [%d, %d]", int(
-                    sheetInd), int(rowInd), int(col))
+                    sheet_ind), int(row_ind), int(col))
                 results[sheet][row - 1][col -
-                                        1] = correctValue(sheetsDict[sheet][1][row][col], col)
+                                        1] = correct_value(sheets_dict[sheet][1][row][col], col)
             results[sheet][-1].extend(dates)
         if (logging.getLogger().level <= logging.DEBUG):
             for e in range(len(results)):
                 debug("Results Sheet[" + str(e) + "]", results[e])
 
     # Checking names for repetitions
-    results = sanityName(results)
+    results = sanity_name(results)
 
     # Analysis
     for sheet in range(len(results)):
         # Iterating through results to see where errors occured
         for row in range(len(results[sheet])):
             for col in range(len(results[sheet][row][:-len(dates)])):
-                mainDisplay.sheetStatus.configure(
-                    text=textSanitize.format(sInd=sheet + 1, sMax=len(results), rInd=row + 1, rMax=len(results[sheet])))
+                Main_Display.sheet_status.configure(
+                    text=text_sanitize.format(
+                        sInd=sheet + 1, sMax=len(results),
+                        rInd=row + 1, rMax=len(results[sheet])))
                 if (results[sheet][row][col][2] == False):
-                    results[sheet][row][col] = mainDisplay.requestCorrection(
-                        sheetsDict[sheet][1][row + 1][col + 1], results[sheet][row][col][0])
+                    results[sheet][row][col] = Main_Display.request_correction(
+                        sheets_dict[sheet][1][row + 1][col + 1], results[sheet][row][col][0])
                     if (col + 1 in [1, 5]):
                         for entry in JSON["names"][str(col + 1)]:
                             if (results[sheet][row][col][0].lower() == entry):
                                 break
                         else:
-                            JSONChange = True
-                            # if the name possibly entered in by the user doesnt exist in the database, add it
+                            JSON_CHANGE = True
+                            # if the name possibly entered in by the user doesnt
+                            # exist in the database, add it
                             JSON["names"][str(
                                 col + 1)].append(results[sheet][row][col][0].lower())
 
         # Checking if any rows are blank
         for row in range(len(results[sheet])-1, -1, -1):
-            if checkBlankRow(results[sheet][row]):
+            if check_blank_row(results[sheet][row]):
                 results[sheet].pop(row)
 
-    if(outputDict == None):
+    if(output_dict == None):
         return results
     else:
-        globals()[outputDict] = results.copy()
+        globals()[output_dict] = results.copy()
         return
 
 
-def arrayToCsv(directory):
-    """takes a matrix and returns a string in CSV format.
-    var directory: a string[][] matrix that contains the information of people at the center.
-    returns: a string that contains all the information in CSV format.
+def array_to_csv(directory):
+    """takes a matrix and returns a string in CSV format.\n
+    @param directory: a string[][] matrix that contains the information of
+    people at the center.\n
+    @return: a string that contains all the information in CSV format.
     """
     cvarray = ''
     for i in range(len(directory)):
@@ -235,36 +244,36 @@ def main():
     ##########################################
 
     try:
-        signinsheet = mainDisplay.signinsheet
-        outputCSV = mainDisplay.outputCSV
-        imageDictionary = imageScraper(signinsheet)
-        debugImageDictionary(imageDictionary)
-        textDictionary = TranslateDictionary(imageDictionary, gui=True)
-        csvString = ""
-        for sheet in textDictionary:
-            csvString += arrayToCsv(sheet)
-        exportToFile(mainDisplay.outputCSV, csvString)
-        mainDisplay.errorLabel.configure(text="All finished.")
+        signinsheet = Main_Display.signinsheet
+        output_CSV = Main_Display.output_CSV
+        image_dictionary = image_scraper(signinsheet)
+        debug_image_dictionary(image_dictionary)
+        text_dictionary = translate_dictionary(image_dictionary, gui=True)
+        csv_string = ""
+        for sheet in text_dictionary:
+            csv_string += array_to_csv(sheet)
+        export_to_file(Main_Display.output_CSV, csv_string)
+        Main_Display.error_label.configure(text="All finished.")
     except BaseException:
         import traceback
-        PopupTag(mainDisplay, "Error", "Looks like something went wrong.\n" +
+        PopupTag(Main_Display, "Error", "Looks like something went wrong.\n" +
                  str(os.sys.exc_info())+"\n"+str(traceback.format_exc()), "#ff0000").run()
         raise
-    PopupTag(mainDisplay, "Done",
+    PopupTag(Main_Display, "Done",
              "Congrats! its all finished.\nLook at your csv and see if it looks alright.").run()
-    if (JSONChange):
+    if (JSON_CHANGE):
         JSON["names"]["1"].sort()  # Sorting new libraries for optimization
         JSON["names"]["5"].sort()
-        JSONFile = open("aliases.json", "w")
-        json.dump(JSON, JSONFile, indent=4, separators=(
+        JSON_file = open("aliases.json", "w")
+        json.dump(JSON, JSON_file, indent=4, separators=(
             ",", ": "), ensure_ascii=True, sort_keys=True)
-        JSONFile.close()
+        JSON_file.close()
 
     # Cleaning old ocr files from tmp
     os.system("del /s /q %tmp%\\tess_*.hocr")
     return
 
 
-mainDisplay = mainGUI(main)
+Main_Display = MainGUI(main)
 if __name__ == "__main__":
-    mainDisplay.run()
+    Main_Display.run()
